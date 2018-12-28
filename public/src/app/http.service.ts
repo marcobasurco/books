937bc1730b77ad0cb26147ca96905b124ae90e15
System Administrator <root@Marcos-MacBook-Pro.local>
2018-12-28 09:06:02 -0800

    added observables

--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -14,11 +14,16 @@ import { HttpClient } from '@angular/common/http';
 export class HttpService {
   constructor(private _http: HttpClient){
     this.getTask();
+    this.getBooks();
   }
 
   getTask(){
-    let tempObservable = this._http.get('/tasks');
+    let tempObservable = this._http.get('/api/v1/authors');
     tempObservable.subscribe(data => console.log('Got our tasks', data));
   }
 
+  getBooks(){
+    let observable = this._http.get('/api/v1/books');
+    observable.subscribe(data => console.log('Got your books', data));
+  }
 }
